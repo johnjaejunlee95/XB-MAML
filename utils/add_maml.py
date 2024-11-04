97438555362d1ad4e9253e12ce2012f845491bdd
johnjaejunlee95 <johnjaejunlee95@unist.ac.kr>
2024-11-04 15:15:45 +0900

    minor updates

--- utils/add_maml.py
+++ utils/add_maml.py
@@ -1,3 +1,80 @@
+# from collections import OrderedDict
+# from copy import deepcopy
+
+# import torch
+
+# from model.conv4 import Conv_Model
+# from model.resnet import ResNet12
+# from method.gradient_utils import Finetuning
+# from method.parameter_utils import sample_model
+# from utils.utils import *
+
+
+# class Add_MAML:
+#     def __init__(self, network):
+#         super(Add_MAML, self).__init__()
+#         self.network = network
+#         self.loss = torch.nn.CrossEntropyLoss()
+#         self.finetuning = Finetuning()
+
+#     def add_maml(self, args, ensemble_model, moving_avg, meta_generators):
+#         sample = self.create_sample_model(args)
+#         model_parameters = self.get_model_parameters(ensemble_model.model)
+#         args.meta_lr = args.multi_meta_lr
+        
+#         if len(ensemble_model.model) < 2:
+#             del ensemble_model.model
+            
+#             ensemble_model.optimizers = None
+#             ensemble_model.model = []
+            
+#             for i in range(2):
+#                 model_n_param = sample_model(args, parameters=model_parameters)
+#                 model_n = self.create_copy_of_sample_model(sample, model_n_param)
+#                 ensemble_model.add_additional_model(args, model_n)  
+                
+#         else:
+#             model_n_param = sample_model(args, parameters=model_parameters)
+#             model_n = self.create_copy_of_sample_model(sample, model_n_param)
+#             ensemble_model.add_additional_model(args, model_n)
+
+#         args.num_maml +=1
+#         moving_avg.values = []
+        
+#         args.avg_dict = {x: Average(x) for x in meta_generators.stat_keys}
+#         args.num_overfitting = 5
+#         self.cleanup(model_n)
+        
+   
+#     def create_sample_model(self, args):
+#         feature_size = args.filter_size*5*5
+#         if args.model == 'conv4':
+#             return Conv_Model(args.imgc, args.num_ways, args.filter_size, feature_size, drop_p=args.dropout).cuda()
+#         else:
+#             return ResNet12(args.num_ways, drop_p=args.dropout).cuda()
+
+#     def get_model_parameters(self, network):
+#         params = OrderedDict()
+
+#         for i in range(len(network)):
+#             model_parameters = OrderedDict(network[i].meta_named_parameters())
+#             for key, values in model_parameters.items():
+#                 if key not in params.keys():
+#                     params[key] = values.detach().cpu() / len(network)
+#                 else:
+#                     params[key] += values.detach().cpu() / len(network)
+#         return params
+
+#     def create_copy_of_sample_model(self, sample, model_n_param):
+#         model_n = deepcopy(sample)
+#         model_n.load_state_dict(model_n_param)
+#         return model_n
+
+#     def cleanup(self, model_n):
+#         del model_n
+#         torch.cuda.empty_cache()
+
+
 from collections import OrderedDict
 from copy import deepcopy
 
@@ -5,71 +82,98 @@ import torch
 
 from model.conv4 import Conv_Model
 from model.resnet import ResNet12
-from method.gradient_utils import Finetuning
 from method.parameter_utils import sample_model
 from utils.utils import *
 
 
 class Add_MAML:
     def __init__(self, network):
-        super(Add_MAML, self).__init__()
+        super().__init__()
         self.network = network
-        self.loss = torch.nn.CrossEntropyLoss()
-        self.finetuning = Finetuning()
+        self._model_creators = {
+            'conv4': lambda args: Conv_Model(
+                args.imgc, 
+                args.num_ways, 
+                args.filter_size, 
+                args.filter_size * 5 * 5, 
+                drop_p=args.dropout
+            ).cuda(),
+            'resnet12': lambda args: ResNet12(
+                args.num_ways, 
+                drop_p=args.dropout
+            ).cuda()
+        }
 
     def add_maml(self, args, ensemble_model, moving_avg, meta_generators):
+        """Add MAML models to the ensemble with improved handling."""
         sample = self.create_sample_model(args)
         model_parameters = self.get_model_parameters(ensemble_model.model)
         args.meta_lr = args.multi_meta_lr
         
+        # Reset ensemble if less than 2 models
         if len(ensemble_model.model) < 2:
-            del ensemble_model.model
-            
-            ensemble_model.optimizers = None
-            ensemble_model.model = []
-            
-            for i in range(2):
-                model_n_param = sample_model(args, parameters=model_parameters)
-                model_n = self.create_copy_of_sample_model(sample, model_n_param)
-                ensemble_model.add_additional_model(args, model_n)  
-                
+            ensemble_model.reset()
+            num_models_to_add = 2
         else:
-            model_n_param = sample_model(args, parameters=model_parameters)
-            model_n = self.create_copy_of_sample_model(sample, model_n_param)
+            num_models_to_add = 1
+            
+        # Add models in batch
+        for _ in range(num_models_to_add):
+            model_n = self._create_and_add_model(args, sample, model_parameters)
             ensemble_model.add_additional_model(args, model_n)
-
-        args.num_maml +=1
-        moving_avg.values = []
+            
+        # Update args and reset moving average
+        self._update_args(args, meta_generators)
+        moving_avg.values.clear()
         
+        # Cleanup
+        del sample
+        torch.cuda.empty_cache()
+
+    def _create_and_add_model(self, args, sample, model_parameters):
+        """Helper method to create and prepare a new model."""
+        model_n_param = sample_model(args, parameters=model_parameters)
+        model_n = self.create_copy_of_sample_model(sample, model_n_param)
+        return model_n
+
+    def _update_args(self, args, meta_generators):
+        """Update arguments after adding models."""
+        args.num_maml += 1
         args.avg_dict = {x: Average(x) for x in meta_generators.stat_keys}
         args.num_overfitting = 5
-        self.cleanup(model_n)
-        
-   
+
     def create_sample_model(self, args):
-        feature_size = args.filter_size*5*5
-        if args.model == 'conv4':
-            return Conv_Model(args.imgc, args.num_ways, args.filter_size, feature_size, drop_p=args.dropout).cuda()
-        else:
-            return ResNet12(args.num_ways, drop_p=args.dropout).cuda()
+        """Create a sample model based on architecture type."""
+        creator = self._model_creators.get(args.model.lower())
+        if not creator:
+            raise ValueError(f"Unsupported model type: {args.model}")
+        return creator(args)
 
-    def get_model_parameters(self, network):
-        params = OrderedDict()
-
-        for i in range(len(network)):
-            model_parameters = OrderedDict(network[i].meta_named_parameters())
-            for key, values in model_parameters.items():
-                if key not in params.keys():
-                    params[key] = values.detach().cpu() / len(network)
-                else:
-                    params[key] += values.detach().cpu() / len(network)
+    def get_model_parameters(self, model_list):
+        """Get averaged model parameters with improved efficiency."""
+        if not model_list:
+            return OrderedDict()
+            
+        # Initialize with first model's parameters
+        params = OrderedDict(model_list[0].meta_named_parameters())
+        params = {k: v.detach().cpu() for k, v in params.items()}
+        
+        # Add remaining models' parameters
+        for model in model_list[1:]:
+            model_params = OrderedDict(model.meta_named_parameters())
+            for key, values in model_params.items():
+                params[key] += values.detach().cpu()
+        
+        # Average the parameters
+        num_models = len(model_list)
+        for key in params:
+            params[key] /= num_models
+            
         return params
 
-    def create_copy_of_sample_model(self, sample, model_n_param):
+    @staticmethod
+    def create_copy_of_sample_model(sample, model_n_param):
+        """Create a copy of the sample model with given parameters."""
         model_n = deepcopy(sample)
         model_n.load_state_dict(model_n_param)
         return model_n
-
-    def cleanup(self, model_n):
-        del model_n
-        torch.cuda.empty_cache()
--- utils/ensemble.py
+++ utils/ensemble.py
@@ -5,44 +5,110 @@ from collections import OrderedDict
 from torchmeta.modules import MetaModule
 from method.parameter_utils import linear_combination
 
+# class Ensemble_Model(MetaModule):
+#     def __init__(self):
+#         super(Ensemble_Model, self).__init__()
+
+#         self.optimizers = None
+#         self.model = []
+
+#     def add_additional_model(self, args, new_network, status='train'):
+       
+#         self.model.append(new_network)
+            
+#         if status == 'train':
+#             if self.optimizers is None:
+#                 self.optimizers = torch.optim.Adam(new_network.parameters(), lr=args.meta_lr)
+#             else:
+#                 self.optimizers.add_param_group({'params': new_network.parameters(), 'lr': args.meta_lr})
+#         else:
+#             pass
+            
+#     def _compute_final_weights(self, args, param_list, train_losses=None):
+#         sigma = F.softmax(-torch.stack(train_losses) / args.temp_scaling, dim=0)
+#         final_weights = linear_combination(param_list, sigma=sigma)
+#         return sigma, final_weights
+
+#     def outputs(self, args, batch=None):
+                
+#         train_losses = []
+#         param_list = []
+#         train_input, train_target = batch
+        
+#         for i in range(len(self.model)):
+#             self.model[i].eval()
+#             logits = self.model[i](train_input)
+#             train_loss = F.cross_entropy(logits, train_target)
+#             self.model[i].zero_grad()
+#             train_losses.append(train_loss)
+#             param_list.append(OrderedDict(self.model[i].meta_named_parameters()))
+
+#         sigmas, weights = self._compute_final_weights(args, param_list, train_losses)
+
+#         return weights, sigmas
+
+
 class Ensemble_Model(MetaModule):
     def __init__(self):
-        super(Ensemble_Model, self).__init__()
-
-        self.optimizers = None
-        self.model = []
-
+        super().__init__()
+        self.optimizer = None
+        self.models = torch.nn.ModuleList()  # Using ModuleList for better model management
+    
     def add_additional_model(self, args, new_network, status='train'):
-       
-        self.model.append(new_network)
-            
+        """Add a new model to the ensemble and configure its optimizer if in training mode.
+        
+        Args:
+            args: Configuration arguments
+            new_network: Neural network model to add
+            status: 'train' or 'eval' mode
+        """
+        self.models.append(new_network)
+        
         if status == 'train':
-            if self.optimizers is None:
-                self.optimizers = torch.optim.Adam(new_network.parameters(), lr=args.meta_lr)
+            if self.optimizer is None:
+                self.optimizer = torch.optim.Adam(new_network.parameters(), lr=args.meta_lr)
             else:
-                self.optimizers.add_param_group({'params': new_network.parameters(), 'lr': args.meta_lr})
-        else:
-            pass
-            
-    def _compute_final_weights(self, args, param_list, train_losses=None):
-        sigma = F.softmax(-torch.stack(train_losses) / args.temp_scaling, dim=0)
+                self.optimizer.add_param_group({'params': new_network.parameters(), 'lr': args.meta_lr})
+
+    @torch.no_grad()  # Optimization: disable gradient computation for inference
+    def _compute_final_weights(self, args, param_list, train_losses):
+        """Compute ensemble weights using softmax with temperature scaling.
+        
+        Args:
+            args: Configuration arguments
+            param_list: List of model parameters
+            train_losses: List of training losses
+        
+        Returns:
+            tuple: (sigma weights, final weighted parameters)
+        """
+        train_losses = torch.stack(train_losses)
+        sigma = F.softmax(-train_losses / args.temp_scaling, dim=0)
         final_weights = linear_combination(param_list, sigma=sigma)
         return sigma, final_weights
 
-    def outputs(self, args, batch=None):
-                
+    def outputs(self, args, batch):
+        """Compute ensemble outputs and weights.
+        
+        Args:
+            args: Configuration arguments
+            batch: Tuple of (input, target)
+        
+        Returns:
+            tuple: (final weights, sigma weights)
+        """
         train_losses = []
         param_list = []
         train_input, train_target = batch
         
-        for i in range(len(self.model)):
-            self.model[i].eval()
-            logits = self.model[i](train_input)
-            train_loss = F.cross_entropy(logits, train_target)
-            self.model[i].zero_grad()
-            train_losses.append(train_loss)
-            param_list.append(OrderedDict(self.model[i].meta_named_parameters()))
+        # Process all models in eval mode
+        with torch.no_grad():  # Optimization: disable gradients during forward pass
+            for model in self.models:
+                model.eval()
+                logits = model(train_input)
+                train_loss = F.cross_entropy(logits, train_target)
+                train_losses.append(train_loss)
+                param_list.append(OrderedDict(model.meta_named_parameters()))
 
         sigmas, weights = self._compute_final_weights(args, param_list, train_losses)
-
         return weights, sigmas
